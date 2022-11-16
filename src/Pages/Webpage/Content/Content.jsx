b5e8f4d099ae3aa5a767c5c9fb9a5b82deec4390
Venkatesan Chandra Sekaran <venkatesan_dinesh@icloud.com>
2022-11-16 18:08:35 +0530

    Zoom Image

--- src/Pages/Webpage/Content/Content.jsx
+++ src/Pages/Webpage/Content/Content.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import "./Content.css"
 export default function Content() {
   return (
-        <img src="https://static.wixstatic.com/media/5dee0c_ffa00a461a7c4ea5930ed5d7784b33fb~mv2.jpg/v1/fill/w_2156,h_1051,al_c,q_90,enc_auto/5dee0c_ffa00a461a7c4ea5930ed5d7784b33fb~mv2.jpg" alt=""/>
+        <img className='data' src="https://static.wixstatic.com/media/5dee0c_ffa00a461a7c4ea5930ed5d7784b33fb~mv2.jpg/v1/fill/w_2156,h_1051,al_c,q_90,enc_auto/5dee0c_ffa00a461a7c4ea5930ed5d7784b33fb~mv2.jpg" alt=""/>
   )
 }
--- src/Pages/Webpage/Display/Display.jsx
+++ src/Pages/Webpage/Display/Display.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./Display.css"
 import Topbar from "../Topbar/WepageTopbar";
-
+import Content from '../Content/Content';
 export default function Display() {
   return (
     <div>
         <Topbar/>
         <div className="container">
+          <Content/>
         <div className="middle-1">
         <h1>DD Lab Grown Diamond COMING SOON</h1>
         <h4 className='hcolor-1'>Product Page is under construction </h4>
