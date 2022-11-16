fc9005201eff05bdf9b5b899832e98e9387ef461
Venkatesan Chandra Sekaran <venkatesan_dinesh@icloud.com>
2022-11-16 18:29:00 +0530

    Cron At top

--- src/Pages/Webpage/Display/Display.jsx
+++ src/Pages/Webpage/Display/Display.jsx
@@ -7,8 +7,8 @@ export default function Display() {
     <div>
         <Topbar/>
         <div className="container">
-          <Content/>
         <div className="middle-1">
+        <img className='data' src="https://www.pngkit.com/png/full/989-9897010_free-png-transparent-diamond-crown-png-image-with.png" alt=""/>
         <h1>DD Lab Grown Diamond COMING SOON</h1>
         <h4 className='hcolor-1'>Product Page is under construction </h4>
         <h6 className='hcolor-1'>We will be back soon</h6>
