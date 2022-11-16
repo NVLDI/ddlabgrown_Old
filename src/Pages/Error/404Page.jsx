20be6a4d54c9d0ed362eaa038f50bc8dc4d97f94
Venkatesan Chandra Sekaran <venkatesan_dinesh@icloud.com>
2022-11-16 18:18:32 +0530

    2nd test for images

--- src/Pages/Error/404Page.jsx
+++ src/Pages/Error/404Page.jsx
@@ -9,8 +9,9 @@ export default function Page() {
     <Link className='link' to={"/"}>
     <h3>Back</h3>
     </Link>
-  </div>
-        <div class="middle">
+    </div>
+    <div class="middle">
+    <img className='data' src="https://www.freepnglogos.com/uploads/diamond-png/diamond-gemstone-black-icon-transparent-png-svg-vector-37.png" alt=""/>
     <h1>DD Lab Grown Diamond COMING SOON</h1>
     <h4 className='hcolor'>Product Page is under construction </h4>
     <h6 className='hcolor'>We will be back soon</h6>
